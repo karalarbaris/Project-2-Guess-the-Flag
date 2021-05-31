482b57455601b58ca6647b85b1a22a3f01bb33e5
Baris <karalarbaris@gmail.com>
2021-05-31 07:51:18 +0200

    Initial commit

---
+++ Project-2-Guess-the-Flag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project-2-Guess-the-Flag
+//
+//  Created by Baris Karalar on 31.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# Project-2-Guess-the-Flag
+ 
